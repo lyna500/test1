9b56c100ebeb2512626da782aa458df6d0583237
lyna500 <lynasidane55@gmail.com>
2026-09-21 17:18:38 +0200

    test5

--- mon-api/index.js
+++ mon-api/index.js
@@ -3,17 +3,19 @@ import express from "express"
 
 function cors(req,res,next){
    res.set('Access-Control-Allow-Origin','*')
-   res.set('Access-Control-Allow-Methodes','*')
-   res.set('Acess-Control-Allow-Headers','Content-Type, x-api-key')
+   res.set('Access-Control-Allow-Methodes', '*')
+   res.set('Access-Control-Allow-Headers', 'Content-Type, x-api-key')
+
    next()
 }
 const app=express()
 app.use(cors)
 app.use(express.json())
+
 app.get("/test/:id", (req, res) => {
    fetch("https://jsonplaceholder.typicode.com/posts/1")
    .then(res  => res.json())
-   .then(data => response.json(data))
+   .then(data => res.json(data))
   
 })
 
