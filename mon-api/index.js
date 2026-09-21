d9e0337a8b0bbff3f5fdb50f71aa60580d2ec791
lyna500 <lynasidane55@gmail.com>
2026-09-21 16:54:41 +0200

    test

--- mon-api/index.js
+++ mon-api/index.js
@@ -10,7 +10,7 @@ function cors(req,res,next){
 const app=express()
 app.use(cors)
 app.use(express.json())
-appapp.get("/test/:id", (req, res) => {
+app.get("/test/:id", (req, res) => {
    fetch("https://jsonplaceholder.typicode.com/posts/1")
    .then(res  => res.json())
    .then(data => response.json(data))
