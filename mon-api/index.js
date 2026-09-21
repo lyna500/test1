b084febd02cefe69819c07a9fffcf9651eaaa80f
lyna500 <lynasidane55@gmail.com>
2026-09-21 16:34:33 +0200

    test

--- mon-api/index.js
+++ mon-api/index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.get("/test", (req, res) => {
    fetch("https://jsonplaceholder.typicode.com/posts/1")
    .then(res  => res.json())
-.then(data => console.log(data))
+.then(data => response.json(data))
   
 })
 
--- my-react-app/src/components/button.jsx
+++ my-react-app/src/components/button.jsx
@@ -1,21 +1,45 @@
-import {useState} from "react";
+
+import { useState, useEffect } from "react";
 
 const Title = () => {
-    const [count, setCount]= useState(0);
-     const [titre, setTitre]= useState("hello world");
-    
+    const [count, setCount] = useState(0);
+    const [titre, setTitre] = useState("hello world");
+
+    const ApiCall = async () => {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts/1"
+        );
+
+        const data = await response.json();
+
+        console.log(data);
+        setTitre(data.body);
+    };
+
+    useEffect(() => {
+        ApiCall();
+    }, []);
+
     return (
-    <div>
-        <h1>(titre)</h1>
-        <p>ceci est composant react tout simple.</p>
-        <p>{count}</p>
-       
-         <button onClick={() => {
-            setCount(count + 1)
+        <div className="gearge">
+            <h1 id="patrick" className="geaorge">
+                {titre}
+            </h1>
 
-         }}> click sur moi</button>
-    </div>
-    )
+            <p>Ceci est un composant React très simple.</p>
 
-}
+            <p className="geaorge">{count}</p>
+
+            <button
+                onClick={() => {
+                    setCount(count + 1);
+                }}
+            >
+                Clique sur moi
+            </button>
+        </div>
+    );
+};
+
 export default Title;
+
