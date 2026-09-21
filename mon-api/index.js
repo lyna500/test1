d908ec8e32dc4736c0fd931054fadf755682dd4a
lyna500 <lynasidane55@gmail.com>
2026-09-21 16:40:13 +0200

    test

--- mon-api/index.js
+++ mon-api/index.js
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 app.use(express.json())
 
-app.get("/test", (req, res) => {
+app.get("/test/:id", (req, res) => {
    fetch("https://jsonplaceholder.typicode.com/posts/1")
    .then(res  => res.json())
 .then(data => response.json(data))
